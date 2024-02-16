42f7be50f3fde0bdffdcbe026c4005714fd0fd8e
Max <139768623+zerotul777@users.noreply.github.com>
2024-02-16 16:30:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,52 +2,7 @@ const inputElement = document.getElementById('title')
 const createBtn = document.getElementById('create')
 const listElement = document.getElementById('list')
 
-// const notes = ['записать блок про массивы', 'рассказать теорию объектов', 'саламалейкум']
-
-// function render() {
-//     // for (let i = 0; i < notes.length; i++) {
-//     //     listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]))
-//     // }
-//     for (let note of notes) {
-//         listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note))
-//     }
-// }
-
-// render()
-
-// createBtn.onclick = function () {
-//     if (inputElement.value.length === 0) {
-//         return
-//     }
-//     listElement.insertAdjacentHTML(
-//         'beforeend', 
-//         getNoteTemplate(inputElement.value)
-//     )
-//     inputElement.value = ''
-//  }
-
-// function getNoteTemplate(title) {
-//     return `
-//         <li class="list-group-item d-flex justify-content-between align-items-center">
-//             <span>${title}</span>
-//             <span>
-//                 <span class="btn btn-small btn-success">&check;</span>
-//                 <span class="btn btn-small btn-danger">&times;</span>
-//             </span>
-//         </li>
-//     `
-// }
-
-const notes = [
-    {
-        title: 'записать блок про массивы',
-        completed: false,
-    },
-    {
-        title: 'рассказать теорию объектов',
-        completed: true,
-    }
-]
+const notes = []
 
 function render() {
     listElement.innerHTML = ''
